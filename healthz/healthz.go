5087200cab5b0f3ab39546be51137dcb85e226a7
Nick Granado <ngranado@gmail.com>
2023-07-26 02:23:39 -0700

    It's `time.Since()`.

--- healthz/healthz.go
+++ healthz/healthz.go
@@ -7,16 +7,16 @@ import (
 )
 
 type Healthz struct {
-	Version   string        `json:"version"`
-	BuildDate string        `json:"build_date"`
-	TimeSince time.Duration `json:"time_since"`
-	Hash      string        `json:"commit_hash"`
+	Version   string `json:"version"`
+	BuildDate string `json:"build_date"`
+	TimeSince string `json:"time_since"`
+	Hash      string `json:"commit_hash"`
 }
 
 func (h *Healthz) Handler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		t, _ := time.Parse("2006-01-02T15:04:05Z0700", h.BuildDate)
-		h.TimeSince = time.Until(t)
+		h.TimeSince = time.Since(t).String()
 		body, _ := json.MarshalIndent(&h, "", "  ")
 		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(http.StatusOK)
