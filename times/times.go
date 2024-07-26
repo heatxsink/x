44169f877100acd5c9cecd0ded4d61125a9624ba
Nick Granado <ngranado@gmail.com>
2024-07-26 15:17:59 -0700

    Added `times`.
    - This module has some nice utility functions for `time.Time` manipulation.

---
+++ times/times.go
@@ -0,0 +1,27 @@
+package times
+
+import "time"
+
+var FriendlyShort = "Mon, Jan 02 15:04 MST"
+
+func ConvertToTimezone(timezone string, ts string) (time.Time, error) {
+	var t time.Time
+	loc, err := time.LoadLocation(timezone)
+	if err != nil {
+		return t, err
+	}
+	t, err = time.Parse(time.RFC3339, ts)
+	if err != nil {
+		return t, err
+	}
+	return t.In(loc), nil
+}
+
+func IsWithinDays(t time.Time, days int) bool {
+	start := time.Now()
+	end := time.Now().AddDate(0, 0, days)
+	if t.After(start) && t.Before(end) {
+		return true
+	}
+	return false
+}
