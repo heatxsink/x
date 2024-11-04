2a951e4c3cf728df766482d46e9628191b81c949
Nick Granado <ngranado@gmail.com>
2024-11-03 16:04:24 -0800

    `times` updates / breakage.
    - I know, I know I'm breaking my promises.
    - `IsWithinDays` expects UTC.
    - `ConvertToTimezone` -> `ConvertToLocation`.
    - Added more `const` time layouts.

--- times/times.go
+++ times/times.go
@@ -3,27 +3,30 @@ package times
 import "time"
 
 const (
-	FriendlyShort = "Mon, Jan 02 15:04 MST"
-	Friendly      = "Mon, Jan 02, 2006 at 15:04 MST"
+	FriendlyShort     = "Mon, Jan 02 15:04 MST"
+	Friendly          = "Mon, Jan 02, 2006 at 15:04 MST"
+	DateDirectory     = "2006.01.02"
+	DateTimeDirectory = "2006.01.02.15.04.05"
+	AlmostRFC3339     = "2006-01-02T15:04:05"
 )
 
-func ConvertToTimezone(timezone string, ts string) (time.Time, error) {
+func ConvertToLocation(layout string, value string, location string) (time.Time, error) {
 	var t time.Time
-	loc, err := time.LoadLocation(timezone)
+	loc, err := time.LoadLocation(location)
 	if err != nil {
 		return t, err
 	}
-	t, err = time.Parse(time.RFC3339, ts)
+	t, err = time.Parse(layout, value)
 	if err != nil {
 		return t, err
 	}
 	return t.In(loc), nil
 }
 
-func IsWithinDays(t time.Time, days int) bool {
-	start := time.Now()
-	end := time.Now().AddDate(0, 0, days)
-	if t.After(start) && t.Before(end) {
+func IsWithinDays(utc time.Time, days int) bool {
+	start := time.Now().UTC()
+	end := time.Now().UTC().AddDate(0, 0, days)
+	if utc.After(start) && utc.Before(end) {
 		return true
 	}
 	return false
